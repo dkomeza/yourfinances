0e3816437b9e9ecf9e2c0b89c782ae513b7aa92c
Dawid Komęza <dawid.komeza@outlook.com>
2022-08-10 18:32:18 +0200

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './components/Auth/context/AuthContext';
+import Signup from './components/Auth/components/Signup';
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <header className="App-header">
-        </header>
-      </AuthProvider>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
