e0eccd823577a7b4585a14ed0ce472295a711dc9
Dawid Komęza <dawid.komeza@gmail.com>
2022-12-17 21:17:26 +0100

    Project reset & switch to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './components/Auth/context/AuthContext';
-import PrivateRoute from './components/Auth/components/PrivateRoute';
-
-import Signup from './components/Auth/components/Signup';
-import Login from './components/Auth/components/Login';
-import InnerRouter from './components/Content/InnerRouter';
-
-
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <AuthProvider>
-          <Routes>
-            <Route path="*" element={<PrivateRoute> <InnerRouter /> </PrivateRoute>}/>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </AuthProvider>
-      </Router>
-    </div>
-  );
+  return <div className="App">supcio</div>;
 }
 
 export default App;
